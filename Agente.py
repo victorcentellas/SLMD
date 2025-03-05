2bbf0057c4e501f6df859783385b4047c66c99a4
victorcentellas <victor.centellas@alu.uclm.es>
2025-03-05 19:28:06 +0100

    Logs y JSON de envios

--- Agente.py
+++ Agente.py
@@ -5,6 +5,7 @@ import time
 import json
 import uuid
 from datetime import datetime
+import threading
 
 # Configuración del broker MQTT
 BROKER = "localhost"
@@ -12,7 +13,6 @@ PORT = 1883
 USERNAME = "user1"
 PASSWORD = "clave123"
 
-# Generar un ID único para cada agente
 AGENT_ID = str(uuid.uuid4())
 
 def get_mock_accelerometer():
@@ -50,29 +50,45 @@ def publish_data(client, topic, data):
     """Publica el JSON en el tópico correspondiente."""
     client.publish(topic, json.dumps(data))
 
+def publish_10DOF(client):
+    while True:
+        metricas_10DOF = {
+            "acelerometro": get_mock_accelerometer(),
+            "giroscopio": get_mock_gyroscope(),
+            "magnetometro": get_mock_magnetometer(),
+            "barometro": get_mock_barometer(),
+        }
+        
+        base_topic = f"sensor/{AGENT_ID}"
+        
+        for sensor, data in metricas_10DOF.items():
+            publish_data(client, f"{base_topic}/{sensor}", data)
+        time.sleep(1)
+        #time.sleep(0.1)  # 10 veces por segundo
+
+def publish_GPS(client):
+    while True:
+        metricas_GPS =  get_mock_gps()
+        base_topic = f"sensor/{AGENT_ID}/gps"
+        publish_data(client, base_topic, metricas_GPS)
+        time.sleep(2)
+
+        #time.sleep(0.2)  # 5 veces por segundo
+
 def main():
     client = mqtt.Client()
     client.username_pw_set(USERNAME, PASSWORD)
     client.connect(BROKER, PORT, 60)
     client.loop_start()
     
+    thread_10DOF = threading.Thread(target=publish_10DOF, args=(client,), daemon=True)
+    thread_GPS = threading.Thread(target=publish_GPS, args=(client,), daemon=True)
+    
+    thread_10DOF.start()
+    thread_GPS.start()
+    
     while True:
-        metricas = {
-            "acelerometro": get_mock_accelerometer(),
-            "giroscopio": get_mock_gyroscope(),
-            "magnetometro": get_mock_magnetometer(),
-            "barometro": get_mock_barometer(),
-            "gps": get_mock_gps()
-        }
-        
-        base_topic = f"sensor/{AGENT_ID}"
-        
-        for sensor, data in metricas.items():
-            publish_data(client, f"{base_topic}/{sensor}", data)
-        
-        print("Publicado en MQTT para el agente:", AGENT_ID)
-        
-        time.sleep(5)
+        time.sleep(1)  # Mantener el programa en ejecución
 
 if __name__ == "__main__":
     main()
